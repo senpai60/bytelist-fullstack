149600757b5a430152311a9ca4210efba6d96e5b
Vivek Satloniya <sathloniyavivek64@gmail.com>
2025-11-01 05:58:05 +0530

    Added--> CORS White list updated **TRY-1**

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-import 'dotenv/config' 
+import "dotenv/config";
 
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import { fileURLToPath } from 'url';
-import cors from "cors"
-import indexRouter from './routes/index.js';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import indexRouter from "./routes/index.js";
 
-import connectDB from './utils/db.js';
+import connectDB from "./utils/db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,14 +16,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 // ==============imports-end================//
 
-
-connectDB()
-
+connectDB();
 
 // === CORS === //
 const whitelist = [
   "http://localhost:5173",
-  "https://bytelist-client.vercel.app"
+  "https://bytelist-client.vercel.app",
+  "https://bytelist-server.onrender.com", // ✅ allow own domain (Render’s HTTPS)
+  "http://bytelist-server.onrender.com", // ✅ allow HTTP redirect too
 ];
 
 const corsOptions = {
@@ -41,13 +41,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 export default app;
